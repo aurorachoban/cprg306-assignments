aece21e386bfc9742953facff50cf714dd6d0a28
Aurora Choban <aurorachoban@gmail.com>
2026-01-22 10:45:59 -0700

    Initial Commit

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h2> Name: Aurora Choban </h2>
+            <p> 
+                <Link href=""
+            </p>
+        </div>
+    );
+}
