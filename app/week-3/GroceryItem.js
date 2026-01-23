fbfd416a778999b14a85fd2a09d28f3f0ef92805
Aurora Choban <aurorachoban@gmail.com>
2026-01-23 11:55:29 -0700

    Adjusted GroceryList Return

--- app/week-3/GroceryItem.js
+++ app/week-3/GroceryItem.js
@@ -1,12 +1,15 @@
 export default function Item({ name, quantity, category }) {
   return (
-    <li className="p-4 bg-stone-200 dark:bg-slate-800 rounded-lg">
+    <li className="p-4 bg-stone-200 dark:bg-slate-800 rounded-lg w-150">
       <div className="flex flex-col">
         <span className="text-lg font-bold text-pink-500 dark:text-pink-300">
           {name}
         </span>
         <span className="text-sm text-gray-600 dark:text-gray-100">
-          Quantity: {quantity}  |   Category: {category}
+          Quantity: {quantity}
+        </span>
+        <span className="text-sm text-gray-600 dark:text-gray-100">
+          Category: {category}
         </span>
       </div>
     </li>
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,7 +2,7 @@ import GroceryItemList from "./GroceryItemList";
 
 export default function Page() {
   return (
-    <main>
+    <main className="flex flex-col items-center text-center justify-center">
       <h1 className="text-3xl font-bold text-pink-300 text-center p-4">
         Shopping List
       </h1>
