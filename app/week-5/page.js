bf43b2fcef50dbfb400489acf155bbca731753ac
Aurora Choban <aurorachoban@gmail.com>
2026-02-12 19:33:10 -0700

    Updated for week 5

--- app/week-5/page.js
+++ app/week-5/page.js
@@ -1,1 +1,17 @@
 import HomeButton from "../components/HomeButton";
+import NewItem from "./NewItem";
+
+export default function Page() {
+    return (
+        <>
+            <HomeButton />
+
+            <main className="flex flex-col items-center text-center justify-center">
+                <h1 className="text-2xl font-bold text-pink-400 text-center p-6">
+                    Basic Interactivity
+                </h1>
+                <NewItem />
+            </main>
+        </>
+    );
+}
