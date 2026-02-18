cad6b614e957fe83960c1fdeca80ae0f2240c64f
Aurora Choban <aurorachoban@gmail.com>
2026-02-17 23:31:48 -0700

    creating week 6 page

---
+++ app/week-6/page.js
@@ -0,0 +1,13 @@
+export default function Page() {
+    return (
+        <>
+
+            <main className="flex flex-col items-center text-center justify-center">
+                <h1 className="text-2xl font-bold text-pink-400 text-center p-6">
+                    Shopping List 3
+                </h1>
+                
+            </main>
+        </>
+    );
+}
