d6e96ef5e92ec7a0c8247da449e260e9af6ab1d1
Aurora Choban <aurorachoban@gmail.com>
2026-03-13 17:58:43 -0600

    reformatting content

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -23,7 +23,7 @@ export default function Page() {
     };
 
     return (
-        <main className="flex flex-col items-center justify-center min-h-screen p-8">
+        <main className="flex flex-col items-center p-8">
             <h1 className="text-2xl font-bold text-pink-400 text-center p-6">
                 Shopping List
             </h1>
